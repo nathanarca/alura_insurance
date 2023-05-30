5b3cee053a29cae58462ffff2af7d8497af62308
Nathan Câmara <nathanarca@outlook.com>
2023-05-29 21:56:54 -0300

    fechando exercicios ecenciais

--- app/DataScience/Main.py
+++ app/DataScience/Main.py
@@ -1,22 +1,34 @@
 import numpy as np
 
-km = np.loadtxt("c:\\NAC\\alura_insurance\\app\\DataScience\\data\\carros-km.txt")
+size = 500000
 
-anos = np.loadtxt("c:\\NAC\\alura_insurance\\app\\DataScience\\data\\carros-anos.txt", dtype=int)
 
-km_media = km / (2023 - anos)
+masculino = 0
+feminino = 1
 
-#type(km_media)
+casado = 1
+solteiro = 2
+divorciado = 3
+viuvo = 4
 
-letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+ids = np.arange(1, size + 1)
 
-a1 = letras[:2]
-print(a1)
+idades = np.random.randint(18, 82, size=size)
 
-a2 = letras[2:5]
+sexos = np.random.choice([masculino, feminino], size=size)
 
-print(a2)
+estados_civis = np.random.choice([solteiro, casado, divorciado, viuvo], size=size)
 
-a3 = letras[-3:]
+sinistros = np.random.randint(0, 50, size=size)
 
-print(a3)
+premios = np.random.uniform(10000, 200000, size=size)
+
+dados = np.column_stack((ids, idades, sexos, estados_civis, sinistros, premios))
+
+clientesMaioresDe30 = dados[dados[:, 1] > 30]
+
+clientesSemSinistro = dados[dados[:, 4] == 0]
+
+clientesSemSinistroCOmPremioMaiorQue1000 =  clientesSemSinistro = clientesSemSinistro[clientesSemSinistro[:, 5] > 1000]
+
+print(clientesSemSinistroCOmPremioMaiorQue1000.size)
